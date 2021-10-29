8bbe90ca7052c02072265f485d943b52f22fdf76
vasukomuravelli <vasukomuravelli96@gmail.com>
2021-10-29 20:29:25 +0530

    Update tailwind.config.js

--- tailwind/dist/tailwind.config.js
+++ tailwind/dist/tailwind.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 module.exports = {
     fill : theme =>({
         'red': theme('colors.red.500'),
@@ -5,4 +6,13 @@ module.exports = {
        'blue': theme('colors.blue.500'),
        'white' : theme('colors.white'),
     })
+=======
+module.exports = {
+    fill : theme =>({
+        'red': theme('colors.red.500'),
+       'green': theme('colors.green.500'),
+       'blue': theme('colors.blue.500'),
+       'white' : theme('colors.white'),
+    })
+>>>>>>> 7bb737717b2c986d9999bc97ca196f83503ec7ad
 }
