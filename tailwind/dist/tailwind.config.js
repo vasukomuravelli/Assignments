5e9ff86a9c0fb85a6735135033749b6fe4b761b9
vasukomuravelli <vasukomuravelli96@gmail.com>
2021-10-29 20:28:03 +0530

    Create tailwind.config.js

---
+++ tailwind/dist/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    fill : theme =>({
+        'red': theme('colors.red.500'),
+       'green': theme('colors.green.500'),
+       'blue': theme('colors.blue.500'),
+       'white' : theme('colors.white'),
+    })
+}
